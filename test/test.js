f684ec15bbbf9ce8634d4bd8ead256567fcc1162
moziliang <1315553730@qq.com>
2020-04-25 22:13:19 +0800

    asda

--- test/test.js
+++ test/test.js
@@ -1,15 +1,16 @@
 
 var bookList = [
     { id: 1, name: '三国演义' },
-    { id: 2, name: '水浒传' },
-    { id: 3, name: '红楼梦' },
-    { id: 4, name: '西游记' },
+    { id: 2, name: '水浒传'},
+    { id: 3, name: '红楼梦'},
+    { id: 4, name: '西游记'},
 ]
 
 var app=new Vue({
     el: "#app",
     data: {
         bookList: bookList,
+        msg:""
 
 
     },
@@ -23,8 +24,21 @@ var app=new Vue({
              id:5,
              name:'金瓶梅',
          })
-        }
-    }
+        },
+        btn1:function(book){
+            if ( book.flagname == undefined ) {
+                this.$set(book, 'flag', false)
+            }
+
+                book.flag=true;
+                console.log(book)
+      },
+      btn2:function(book){
+            
+                book.flag=false
+            
+      }
+  }
 });
 
 
